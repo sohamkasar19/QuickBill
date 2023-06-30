78bfb1d3a86efe4ce5aa89bf9cc18285308d36de
Soham Kasar <soham@Sohams-Air.attlocal.net>
2023-06-29 23:22:28 -0700

    add checks and improved UI

--- src/components/Login.js
+++ src/components/Login.js
@@ -6,6 +6,7 @@ import {
   TextInput,
   StyleSheet,
   TouchableOpacity,
+  Alert,
 } from 'react-native';
 // import {auth} from '../../firebase';
 import auth from '@react-native-firebase/auth';
@@ -15,6 +16,24 @@ const Login = ({navigation}) => {
   const [password, setPassword] = useState('');
 
   const onLogin = () => {
+    // Check for empty fields
+    if (email === '' || password === '') {
+      Alert.alert('Error', 'Please fill all fields');
+      return;
+    }
+
+    // Check for valid email
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    if (!emailRegex.test(email)) {
+      Alert.alert('Error', 'Please enter a valid email');
+      return;
+    }
+
+    // Check for password length
+    if (password.length < 6) {
+      Alert.alert('Error', 'Password should be at least 6 characters');
+      return;
+    }
     auth()
       .signInWithEmailAndPassword(email, password)
       .then(userCredential => {
@@ -42,7 +61,9 @@ const Login = ({navigation}) => {
         secureTextEntry={true}
         onChangeText={setPassword}
       />
-      <Button onPress={onLogin} title="Login" />
+      <TouchableOpacity style={styles.button} onPress={onLogin}>
+        <Text style={styles.buttonText}>Login</Text>
+      </TouchableOpacity>
 
       <TouchableOpacity onPress={() => navigation.navigate('Register')}>
         <Text style={styles.linkText}>First time user? Register</Text>
@@ -58,15 +79,32 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
+    width: '100%',
+    height: 50,
+    padding: 10,
     borderWidth: 1,
-    marginBottom: 10,
-    paddingLeft: 8,
+    borderColor: 'gray',
+    borderRadius: 5,
+    marginBottom: 20,
+    backgroundColor: 'white',
+    fontSize: 15,
   },
   linkText: {
     color: '#0000EE', // Change this to your preferred link color
     textDecorationLine: 'underline',
+    fontSize: 15,
+  },
+  button: {
+    backgroundColor: '#3897f1',
+    width: '100%',
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 5,
+    marginBottom: 30,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,19 +1,57 @@
 import React, {useState} from 'react';
-import {View, TextInput, Button, StyleSheet} from 'react-native';
+import {
+  View,
+  TextInput,
+  Button,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  Alert,
+} from 'react-native';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 
 // import {auth} from '../../firebase';
 
-const Register = () => {
+const Register = ({navigation}) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [name, setName] = useState('');
 
   const onRegister = () => {
+    // Check for empty fields
+    if (email === '' || name === '' || password === '') {
+      alert('Please fill all fields');
+      return;
+    }
+
+    // Check for valid email
+    const emailRegex = /^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/;
+    if (!emailRegex.test(email)) {
+      alert('Please enter a valid email');
+      return;
+    }
+
+    // Check for password length
+    if (password.length < 6) {
+      alert('Password should be at least 6 characters');
+      return;
+    }
     auth()
       .createUserWithEmailAndPassword(email, password)
       .then(userCredential => {
         // The user has been registered and is signed in!
         var user = userCredential.user;
+        firestore().collection('users').doc(userCredential.user.uid).set({
+          email: userCredential.user.email,
+          role: 'user',
+          name: name,
+        });
+        Alert.alert(
+          'Registration Successful',
+          'You are now registered and logged in',
+          [{text: 'OK', onPress: () => navigation.navigate('Home')}],
+        );
         console.log('User registered: ', user.email);
       })
       .catch(error => {
@@ -31,11 +69,18 @@ const Register = () => {
       />
       <TextInput
         style={styles.input}
+        placeholder="Name"
+        onChangeText={setName}
+      />
+      <TextInput
+        style={styles.input}
         placeholder="Password"
         secureTextEntry={true}
         onChangeText={setPassword}
       />
-      <Button onPress={onRegister} title="Register" />
+      <TouchableOpacity style={styles.button} onPress={onRegister}>
+        <Text style={styles.buttonText}>Register</Text>
+      </TouchableOpacity>
     </View>
   );
 };
@@ -47,11 +92,27 @@ const styles = StyleSheet.create({
     padding: 16,
   },
   input: {
-    height: 40,
-    borderColor: 'gray',
+    width: '100%',
+    height: 50,
+    padding: 10,
     borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    marginBottom: 20,
+    backgroundColor: 'white',
+    fontSize: 15,
+  },
+  button: {
+    backgroundColor: '#3897f1',
+    width: '100%',
+    padding: 15,
+    alignItems: 'center',
+    borderRadius: 5,
     marginBottom: 10,
-    paddingLeft: 8,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
 
